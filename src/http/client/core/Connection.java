f4c2b692d72ff49b02973e96356950781dce9d30
Pierre Chat <pierre.chat@etu.univ-lyon1.fr>
2017-05-31 08:56:30 +0200

    Connexion

--- src/http/client/core/Connection.java
+++ src/http/client/core/Connection.java
@@ -1,5 +1,20 @@
 package http.client.core;
 
-public class Connection {
-    
+import java.net.InetAddress;
+
+public class Connection implements Runnable {
+    InetAddress address;
+    int port;
+    String file;
+
+    public Connection(InetAddress address, int port, String file) {
+        this.address = address;
+        this.port = port;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
